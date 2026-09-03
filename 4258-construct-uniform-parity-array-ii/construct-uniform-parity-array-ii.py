efa0afc0cd3b3593b665f6c141428d8004181b06
Bhumika Kumari <bhumikakumari_202410101150705@srmu.ac.in>
2026-09-04 01:40:14 +0530

    Time: 15 ms (100.00%) | Memory: 21.8 MB (60.00%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        minimum=min(nums1)
+        if minimum%2==1:
+            return True
+        for num in nums1:
+            if num%2==1:
+                
+                return False
+        
+        return True  
+        """
+        :type nums1: List[int]
+        :rtype: bool
+        """
+        
